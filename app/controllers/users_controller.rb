5d12036a7adf615279115f57e5db8b31c1ab9370
Mark Streltsin <streltsin.mark@gmail.com>
2024-02-23 13:05:22 +0200

    Add user show page with user's articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -26,6 +27,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
 
 
 
