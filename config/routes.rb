776d982031fbcf43ff8f7c5a8cd61d63f6267847
Mark Streltsin <streltsin.mark@gmail.com>
2024-02-25 20:09:22 +0200

    Add login & logout and seesions controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   resources :articles
   resources :users, except: [:new]
   get 'signup', to: 'users#new'
-  # post 'users', to: 'users#create'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
