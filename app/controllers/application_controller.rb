ce6723951c6acd6897c1a708f4c1f6fec633fcf0
Mark Streltsin <streltsin.mark@gmail.com>
2024-02-27 18:40:53 +0200

    Restrict acctions from vies layer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id] # if @current_user isn't nil then return it, otherwise find the user.
+  end
+
+  def logged_in?
+    !!current_user #  !! turning the object into a boolean
+  end
+
 end
