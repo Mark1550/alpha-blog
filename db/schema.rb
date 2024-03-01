1fee522c21a28471f983fa470baea18cf4274da1
Mark Streltsin <streltsin.mark@gmail.com>
2024-03-01 18:00:43 +0200

    Added category model and new page

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_27_173005) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_29_174450) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -27,5 +33,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_27_173005) do
     t.string "password_digest"
     t.boolean "admin", default: false
   end
-  
+
 end
