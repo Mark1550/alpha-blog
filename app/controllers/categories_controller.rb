bed9d895d868c84d13423d0b1aa66f0096e50586
Mark Streltsin <streltsin.mark@gmail.com>
2024-03-01 20:37:29 +0200

    Edit category and cleanup

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,15 +1,27 @@
 class CategoriesController <ApplicationController
   before_action :require_admin, except: [:index, :show]
+  before_action :set_category, only: %I[show edit update]
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
   end
 
   def show
-    @category = Category.find(params[:id])
+    @articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def new
     @category = Category.new
+  end
+
+  def edit; end
+
+  def update
+    if @category.update(category_params)
+      flash[:notice] = 'Category was successfully updated'
+      redirect_to category_path(@category)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def create
@@ -33,5 +45,9 @@ class CategoriesController <ApplicationController
       flash[:alert] = 'Only admins can perform that action'
       redirect_to categories_path
     end
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
   end
 end
